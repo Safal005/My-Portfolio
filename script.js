b22e962322a87377c0a42f224cd458cbb4fce01d
Safal005 <khanalsafal5@gmail.com>
2026-05-10 20:44:48 +0545

    Add scroll-direction detection to hide/show header.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
     const closeIcon = document.querySelector('.close-icon');
     const mobileMenu = document.getElementById('mobile-menu-overlay');
     const navLinks = document.querySelectorAll('.mobile-nav-links a')
+    const header=document.querySelector('header');
     
     menuIcon.addEventListener('click', () => {
         mobileMenu.classList.add('active');
@@ -15,4 +16,14 @@
         link.addEventListener('click', ()=>{
             mobileMenu.classList.remove('active');
         })
+    })
+    let lastScrollY = window.scrollY;
+    window.addEventListener('scroll',()=>{
+        if(window.scrollY>lastScrollY && window.scrollY>100){
+            header.classList.add('header-hidden');
+        }
+        else{
+            header.classList.remove('header-hidden');
+        }
+        lastScrollY=window.scrollY;
     })
